bcb3b49d024761e8a2902b721679e27fcd046082
chenrong <chenrong03@kuaishou.com>
2020-09-13 21:19:27 +0800

    feat: add task demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import 'teambition-sdk/apis/task';
+import { SDK } from 'teambition-sdk';
+import {ProjectId, TaskId} from 'teambition-types';
+import { of } from 'rxjs/observable/of'
+import { useObservable } from 'rxjs-hooks';
 
 function App() {
+  const sdk = new SDK();
+
+  const task = sdk.getTask('5ce399269705060001f15011' as TaskId)
+      .values()
+      .catch((error) => {
+        console.error(error);
+        return of("任务出错了")
+      })
+      .publishReplay(1)
+      .refCount()
+
+  const subTask = sdk.getTask('5f5dd60fda062b0044f8c54a' as TaskId)
+      .values()
+      .catch((error) => {
+          console.error(error);
+          return of("子任务出错了")
+      })
+      .publishReplay(1)
+      .refCount();
+
+  const project = sdk.getProject('5ce39925dd03e00018cd6c20' as ProjectId).values()
+      .catch(error => {
+          console.error(error);
+          return of('任务出错了')
+      })
+      .publishReplay(1)
+      .refCount();
+
+  project.subscribe(() => console.log('Project'))
+  task.subscribe(() => console.log('get task'));
+  subTask.subscribe(() => console.log('get sub task'));
+
+  // sdk.updateTask('5ce39925dd03e00018cd6c20')
+  // project.subscribe();
+
   return (
     <div className="App">
       <header className="App-header">
